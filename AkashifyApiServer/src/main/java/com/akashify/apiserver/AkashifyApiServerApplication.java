783fbccf9c6528fc1844d1114fdcc15d7c4eefb0
Mário Monteiro <mail@koakh.com>
2017-04-13 19:24:51 +0100

    Adding Files

---
+++ AkashifyApiServer/src/main/java/com/akashify/apiserver/AkashifyApiServerApplication.java
@@ -0,0 +1,76 @@
+package com.akashify.apiserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+
+
+@SpringBootApplication
+@EnableOAuth2Sso
+@RestController
+public class AkashifyApiServerApplication extends WebSecurityConfigurerAdapter {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(AkashifyApiServerApplication.class);
+
+  public static void main(String[] args) {
+    SpringApplication.run(AkashifyApiServerApplication.class, args);
+  }
+
+
+  @RequestMapping(value = "/user")
+  public Principal user(Principal principal) {
+    return principal;
+  }
+
+  /**
+   * FIXME: make this as authorized
+   *
+   * @param request
+   * @return
+   */
+  @RequestMapping(value = "/appConfig", method = RequestMethod.GET)
+  public
+  @ResponseBody
+  AppEnvironment appConfig(HttpServletRequest request) {
+
+    LOGGER.debug("Getting Application Config");
+
+    AppEnvironment appEnvironment = new AppEnvironment();
+
+    String kyeCloakUrl = System.getenv("KEYCLOAK_URL");
+    kyeCloakUrl = kyeCloakUrl == null ? "http://koakh.com:8082" : kyeCloakUrl;
+
+    LOGGER.info("Using Key Cloak URL : {}", kyeCloakUrl);
+
+    appEnvironment.setKeyCloakUrl(kyeCloakUrl);
+
+    String redirectUri = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort();
+
+    appEnvironment.setRedirectUri(redirectUri);
+    return appEnvironment;
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+        .antMatcher("/**").authorizeRequests().antMatchers("/", "/appConfig", "/login/**", "/webjars/**")
+        .permitAll().anyRequest()
+        .authenticated().and().exceptionHandling()
+        .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/")).and().logout()
+        .logoutSuccessUrl("/").permitAll()
+        .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+  }
+}
---
+++ AkashifyApiServer/src/main/java/com/akashify/apiserver/AppEnvironment.java
@@ -0,0 +1,25 @@
+package com.akashify.apiserver;
+
+/**
+ * @author kameshs
+ */
+public class AppEnvironment {
+    private String keyCloakUrl;
+    private String redirectUri;
+
+    public String getKeyCloakUrl() {
+        return keyCloakUrl;
+    }
+
+    public void setKeyCloakUrl(String keyCloakUrl) {
+        this.keyCloakUrl = keyCloakUrl;
+    }
+
+    public String getRedirectUri() {
+        return redirectUri;
+    }
+
+    public void setRedirectUri(String redirectUri) {
+        this.redirectUri = redirectUri;
+    }
+}
